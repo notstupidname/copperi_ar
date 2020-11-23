77e678595b10f5bc39126b12695b71d0933a8365
notstupidname <novikov.grigoriy@gmail.com>
2020-11-23 12:15:46 +0300

    1

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,8 +7,20 @@
         arbutton.addEventListener('click', function(e){
             e.preventDefault();
             e.stopPropagation();
-            console.log("click");
+            const arsrc = this.getAttribute('data-arsrc');
+            console.log(arsrc);
             
+            // check for AR support
+            const a = document.createElement('a'),
+                  supportsAR = a.relList.supports('ar') ? true : false;
+            if (supportsAR) {
+                // IPHONE
+            } else {
+                const anchor = document.createElement('a');
+                const href = 'intent://arvr.google.com/scene-viewer/1.0?file=https://ar.notstupid.name/assets/models/' + arsrc + '&mode=ar_only&resizable=false#Intent;scheme=https;package=com.google.ar.core;action=android.intent.action.VIEW;S.browser_fallback_url=https://developers.google.com/ar;end;'
+                anchor.setAttribute('href', href);
+                anchor.click();
+            }
         }, false);
     }
 
