998e9b8721a170313d137db7c4797d2abd01dc22
notstupidname <novikov.grigoriy@gmail.com>
2020-11-23 12:29:24 +0300

    1

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,10 +15,16 @@
                   supportsAR = a.relList.supports('ar') ? true : false;
             if (supportsAR) {
                 // IPHONE
+                const anchor = document.createElement('a');
+                anchor.setAttribute('rel', 'ar');
+                anchor.appendChild(document.createElement('img'));
+                const usdzSrc = arsrc + '.usdz';
+                anchor.setAttribute('href', usdzSrc);
+                anchor.click();
             } else {
                 const anchor = document.createElement('a');
-                const href = 'intent://arvr.google.com/scene-viewer/1.0?file=https://ar.notstupid.name/assets/models/' + arsrc + '&mode=ar_only&resizable=false#Intent;scheme=https;package=com.google.ar.core;action=android.intent.action.VIEW;S.browser_fallback_url=https://developers.google.com/ar;end;';
-                anchor.setAttribute('href', href);
+                const intent = 'intent://arvr.google.com/scene-viewer/1.0?file=https://ar.notstupid.name/assets/models/' + arsrc + '.glb&mode=ar_only&resizable=false#Intent;scheme=https;package=com.google.ar.core;action=android.intent.action.VIEW;S.browser_fallback_url=https://developers.google.com/ar;end;';
+                anchor.setAttribute('href', intent);
                 anchor.click();
             }
         }, false);
