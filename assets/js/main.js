26e3383454d6885f6c8cd261531b0160ec074c85
notstupidname <novikov.grigoriy@gmail.com>
2020-11-23 12:23:07 +0300

    1

--- assets/js/main.js
+++ assets/js/main.js
@@ -17,7 +17,7 @@
                 // IPHONE
             } else {
                 const anchor = document.createElement('a');
-                const href = 'intent://arvr.google.com/scene-viewer/1.0?file=https://ar.notstupid.name/assets/models/' + arsrc + '&mode=ar_only&resizable=false#Intent;scheme=https;package=com.google.ar.core;action=android.intent.action.VIEW;S.browser_fallback_url=https://developers.google.com/ar;end;'
+                const href = 'intent://arvr.google.com/scene-viewer/1.0?file=https://ar.notstupid.name/assets/models/' + arsrc + '&mode=ar_only&resizable=false#Intent;scheme=https;package=com.google.ar.core;action=android.intent.action.VIEW;S.browser_fallback_url=https://developers.google.com/ar;end;';
                 anchor.setAttribute('href', href);
                 anchor.click();
             }
